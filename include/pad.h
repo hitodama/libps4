6744da49c21a2dedb2cc61c241e772df2f291759
hitodama <hitodama@noreply.github.com>
2015-12-29 23:48:46 +0100

    Removed some uncommon headers lost in uncommited generator (may readd)

--- include/pad.h
+++ include/pad.h
@@ -1,7 +1,7 @@
 #pragma once
 
 int scePadInit(void);
-int scePadOpe(int userID, int, int, void *);
+int scePadOpen(int userID, int, int, void *);
 int scePadClose(int handle);
 int scePadRead(int handle, void *data, int count);
 int scePadReadState(int handle, void *data);
