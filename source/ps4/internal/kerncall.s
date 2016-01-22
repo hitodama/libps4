4e316632d0f74d5e3c7d1017e3c8b3858ca14c94
hitodama <hitodama@noreply.github.com>
2016-01-22 08:39:38 +0100

    fixed kerncall & direct mode calls to return -1 instead of errno (no errno set)

--- source/ps4/internal/kerncall.s
+++ source/ps4/internal/kerncall.s
@@ -7,6 +7,12 @@ ps4Kerncall:
 	movq $0x9263FFFF8, %r11
 	movq (%r11), %r11
 	movq %rcx, %r10
-	jmp *%r11
+	call *%r11
+	jnc ps4KerncallR
+	# Be aware that direct mode does not set errno
+	# "neg %rax" would work too - but break more
+	movq $-1, %rax
+ps4KerncallR:
+	ret
 .size ps4Kerncall, .-ps4Kerncall
 .popsection
